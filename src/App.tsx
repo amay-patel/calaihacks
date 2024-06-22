b127c50f21d1ea5475ad7b73949f16221b39b92b
unknown <randomtrooper@protonmail.com>
2024-06-22 14:48:34 -0700

    children's font

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,40 @@
 import React from "react";
 import "./App.css";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
 import StoryCreator from "./components/StoryCreator";
 import { ApiKeyModal } from "./components/ApiKeyModal";
+import { Global } from "@emotion/react";
+
+// Custom font face definition
+const Fonts = () => (
+    <Global
+        styles={`
+      @font-face {
+        font-family: 'Funkids';
+        src: url('/fonts/Funkids.ttf') format('truetype');
+        font-weight: normal;
+        font-style: normal;
+      }
+    `}
+    />
+);
+
+// Extend the Chakra theme to use Funkids as the default font
+const theme = extendTheme({
+    fonts: {
+        body: "Funkids, sans-serif",
+        heading: "Funkids, sans-serif",
+    },
+});
 
 function App() {
-  return (
-    <ChakraProvider>
-      <ApiKeyModal />
-      <StoryCreator />
-    </ChakraProvider>
-  );
+    return (
+        <ChakraProvider theme={theme}>
+            <Fonts />
+            <ApiKeyModal />
+            <StoryCreator />
+        </ChakraProvider>
+    );
 }
 
 export default App;
